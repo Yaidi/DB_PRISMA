fc638ff0de678df957f9440f7ca3cc6393c0554d
Jhoan Garcia <jhoanegar@gmail.com>
2021-03-18 20:02:27 -0600

    enable cors

--- router.js
+++ router.js
@@ -8,6 +8,11 @@ import { createUser, signIn } from 'app/user';
 const swaggerDoc = YAML.load('./swagger.yaml');
 const router = express.Router();
 
+router.use((req, res, next) => {
+  res.append('access-control-allow-origin', '*');
+  next();
+});
+
 // Swagger
 router.use('/', swaggerUi.serve);
 router.get('/', swaggerUi.setup(swaggerDoc));
